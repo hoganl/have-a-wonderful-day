9598ec4cd81f3144cad668326718216c90e176c1
Michael Sklepowich <michaelsklepowich@Michaels-MacBook-Pro-2.local>
2018-02-26 16:39:25 -0800

    fixing typos

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 'use strict';
 
-// random fuction that returns a random value from an array regardless of length
+// returns a random value from an array regardless of length
 
 function random(arrayName) {
   var i = Math.floor(Math.random() * arrayName.length);
-  //console.log(i);
   var outcome = arrayName[i];
   return outcome;
 }
