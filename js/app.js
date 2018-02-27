5461c2aeb5cba43ea09d1c93ba11ccc0da7367b6
Michael Sklepowich <michaelsklepowich@Michaels-MacBook-Pro-2.local>
2018-02-26 17:34:54 -0800

    eexpirementing with initial object literal

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var scene1 = {
+  name = 'park',
+  text = '',
+  option1Text = '',
+  option2Text = '',
+  button1Outcomes = ['good1', 'good2', 'bad1', 'bad2'],
+  button2Outcomes = ['good1', 'good2', 'bad1', 'bad2'],
+  happinessValue = 0,
+}
+
 // returns a random value from an array regardless of length
 
 function random(arrayName) {
