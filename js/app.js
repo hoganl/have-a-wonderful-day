d0a2f2e28d926d65409c9a77d594af63ca96ff9f
Michael Sklepowich <michaelsklepowich@Michaels-MacBook-Pro-2.local>
2018-02-27 11:31:29 -0800

    creating initial object literal for scene 1

--- js/app.js
+++ js/app.js
@@ -1,13 +1,62 @@
 'use strict';
 
+var happinessValue = 0;
+//used for intermediate random generator
+var goodBadArray = ['Good', 'Bad'];
+//scene 1 arrays
+var colorArray = ['orange', 'blue', 'pink', 'purple'];
+var animalArray = ['dog', 'honey badger', 'elephant', 'trash panda'];
+var petOutcomeGood = ['The ' + animal + ' is friendly!  You have a great time playing with the + (random animal) + and then continue with your lovely day!', 'The ' + animal + ' really enjoys the attention and decides to follow you home!  Good luck with that!'];
+var petOutcomeBad = ['Watch out!  The' + animal + ' bites your hand.  You are gonna need a big bandaid!', 'Great, now you are going to smell like ' + animal + ' all day!'];
+var jogOutcomeGood = ['The ' + animal + ' looks sad but waves goodbye as you pass.  You literally have a heart of stone.', 'The ' + animal + 'scratches its back on a tree and only wishes it could be as cool as you.'];
+var jogOutcomeBad =  ['The ' + animal + ' starts to chase you.  You better speed up slow poke!', 'The ' + animal + ' bites your foot as you run past.  That\'s gonna leave a mark!'];
+//scene 2 arrays
+var vehicleArray = ['moped', 'trolly', 'bus', 'volkswagen beetle'];
+var sidewalkArray = ['bicyclist', 'skateboarder', 'scooter', 'runner'];
+//scene 3 arrays
+var hotArray = ['black coffee', 'americana', 'vanlla late', 'black tea'];
+var coldArray = ['fruit smoothie', 'iced coffee', 'protien shake', 'iced tea'];
+//scene 4 arrays
+var groceryArray = ['unicorn meat', 'dragon meat', 'bublegum meatballs', 'kangaroo jerky', 'jellyfish noodles', 'spam snacks', 'smoked rattlesnake'];
+var birdsArray = ['cockatoos', 'seaguls', 'penguins', 'owls'];
+
+var outcomeArray = [];
+
+
+
 var scene1 = {
   name = 'park',
-  text = '',
-  option1Text = '',
-  option2Text = '',
-  button1Outcomes = ['good1', 'good2', 'bad1', 'bad2'],
-  button2Outcomes = ['good1', 'good2', 'bad1', 'bad2'],
-  happinessValue = 0,
+  //subject to change
+  filepath = 'park.jpg',
+  //this is the story text
+  var color = random(colorArray),
+  var animal = random(animalArray),
+  text = 'You are in a park and you see a ' + color + ' ' + animal + '... etc',
+  option1Text = 'Pet the ' + animal + '.',
+  option2Text = 'Keep jogging.',
+  //might need to move this to event handler to change happinessValue
+  outcome1Text = random('petOutcome' + random(goodBadArray)),
+  outcome2Text = random('jogOutcome' + random(goodBadArray)),
+}
+
+
+// event handler & local storage for user name
+
+var nameForm = document.getElementById('nameForm');
+var nameInput = document.getElementById('nameInput');
+var userName = '';
+
+nameForm.addEventListener('submit', updateUserName);
+
+function updateUserName(e) {
+  e.preventDefault();
+  userName = nameInput.value;
+  localStorage.setItem('userName', JSON.stringify(userName));
+  e.target.reset();
+}
+
+if (localStorage.getItem('userName')) {
+  userName = JSON.parse(localStorage.getItem('userName'));
 }
 
 // returns a random value from an array regardless of length
