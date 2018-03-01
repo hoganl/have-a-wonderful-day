33b94adb48b0e42506c0a9283fd617ee511adf0c
vibecastor <vibecastor@gmail.com>
2018-02-28 18:10:39 -0800

    adding canvas writing final code

--- js/app.js
+++ js/app.js
@@ -139,7 +139,7 @@ function renderSceneP1() {
   scenes.style.display = 'block';
   optionButtons.style.display = 'block';
   nextSceneButton.style.display = 'none';
-  scenes.style.backgroundImage = "url('" + Scene.scenesArray[currentScene].filepath + "')";
+  scenes.style.backgroundImage = 'url(\'' + Scene.scenesArray[currentScene].filepath + '\')';
   pNarrative.textContent = Scene.scenesArray[currentScene].narrative;
   option1Button.textContent = Scene.scenesArray[currentScene].option1Text;
   option2Button.textContent = Scene.scenesArray[currentScene].option2Text;
@@ -177,58 +177,92 @@ function random(arrayName) {
   return outcome;
 }
 
-// function writeEffect(canvasId, text) {
+function lineSplit(outcome, lineLength) {
+  var chunksArray = [];
+  var newChunk = outcome + ' ';
+  while (newChunk) {
+    var outSlice = newChunk.slice(0, lineLength);
+    var checkLast = outSlice.lastIndexOf(' ');
+    var firstChunk = outSlice.slice(0, checkLast);
+    newChunk = newChunk.slice(checkLast + 1);
+    chunksArray.push(firstChunk);
+  }
+  return chunksArray;
+}
 
-//   var canvas = document.getElementById(canvasId);
-//   // get 2D context
-//   var ctx = canvas.getContext('2d'),
-//     // dash-length for off-range
-//     dashLen = 350,
-//     // we'll update this, initialize
-//     dashOffset = dashLen,
-//     // some arbitrary speed
-//     speed = 60,
-//     // the text we will draw
-//     txt = canvas.innerHTML = text,
-//     // start position for x and iterator
-//     x = 0, i = 0;
-//   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-//   ctx.font = '50px Comic Sans MS, cursive, TSCu_Comic, sans-serif';
-//   // thickness of the line
-//   ctx.lineWidth = 5;
-//   // to avoid spikes we can join each line with a round joint
-//   ctx.lineJoin = 'round';
-//   ctx.strokeStyle = ctx.fillStyle = 'black';
-//   (function loop() {
-//     // clear canvas for each frame
-//     ctx.clearRect(x, 0, 60, 150);
-//     // calculate and set current line-dash for this char
-//     ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]);
-//     // reduce length of off-dash
-//     dashOffset -= speed;
-//     // draw char to canvas with current dash-length
-//     ctx.strokeText(txt[i], x, 90);
-//     // char done? no, the loop
-//     if (dashOffset > 0) requestAnimationFrame(loop);
-//     else {
-//       // ok, outline done, lets fill its interior before next
-//       //ctx.fillText(txt[i], x, 90);
-//       // reset line-dash length
-//       dashOffset = dashLen;
-//       // get x position to next char by measuring what we have drawn
-//       x += ctx.measureText(txt[i++]).width + ctx.lineWidth;
-//       // if we still have chars left, loop animation again for this char
-//       if (i < txt.length) requestAnimationFrame(loop);
-//     }
-//   })(); // just to self-invoke the loop
-// }
+var narrative = '';
 
-// writeEffect('canvas1', 'The random animal is friendly!');
+var bees = lineSplit(narrative, 50);
 
-// setTimeout (function() {
-//   writeEffect('canvas2', 'You have a great time playing with the random');
-// }, 3500);
+function createCanvas() {
+  var canvasNames = [];
+  for (var i = 0; i < bees.length; i++) {
+    var canvasEl = document.createElement('canvas');
+    canvasEl.id = 'scenario-line-' + i;
+    canvasEl.width = 1200;
+    canvasEl.height = 150;
+    scenes.appendChild(canvasEl);
+    var idOfCanvas = 'scenario-line-' + i;
+    canvasNames.push(idOfCanvas);
+  }
+  return canvasNames;
+}
 
-// setTimeout(function() {
-//   writeEffect('canvas3', 'animal and then continue with your lovely day!');
-// },8200);
+var calling = createCanvas();
+
+function writeEffect(canvasId, text) {
+
+  var canvas = document.getElementById(canvasId);
+  // get 2D context
+  var ctx = canvas.getContext('2d'),
+    // dash-length for off-range
+    dashLen = 550,
+    // we'll update this, initialize
+    dashOffset = dashLen,
+    // some arbitrary speed
+    speed = 300,
+    // the text we will draw
+    txt = canvas.innerHTML = text,
+    // start position for x and iterator
+    x = 20, i = 0;
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.font = '40px Bradley Hand, cursive';
+  // thickness of the line
+  ctx.lineWidth = 5;
+  // to avoid spikes we can join each line with a round joint
+  ctx.lineJoin = 'round';
+  ctx.strokeStyle = ctx.fillStyle = 'black';
+  (function loop() {
+    // clear canvas for each frame
+    ctx.clearRect(x, 0, 60, 150);
+    // calculate and set current line-dash for this char
+    ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]);
+    // reduce length of off-dash
+    dashOffset -= speed;
+    // draw char to canvas with current dash-length
+    ctx.strokeText(txt[i], x, 90);
+    // char done? no, the loop
+    if (dashOffset > 0) requestAnimationFrame(loop);
+    else {
+      // ok, outline done, lets fill its interior before next
+      //ctx.fillText(txt[i], x, 90);
+      // reset line-dash length
+      dashOffset = dashLen;
+      // get x position to next char by measuring what we have drawn
+      x += ctx.measureText(txt[i++]).width + ctx.lineWidth;
+      // if we still have chars left, loop animation again for this char
+      if (i < txt.length) requestAnimationFrame(loop);
+    }
+  })(); // just to self-invoke the loop
+}
+
+var i = 0;
+writeEffect(calling[i], bees[i]);
+i++;
+var thing = setInterval(function () {
+  writeEffect(calling[i], bees[i]);
+  i++;
+  if (i >= bees.length) {
+    clearInterval(thing);
+  }
+}, 1500);
