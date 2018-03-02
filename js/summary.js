8840fbc97f8b628aaca2e01d67435f77e518ef78
danielvportlandia <danielshelton90@gmail.com>
2018-03-02 10:44:35 -0800

    Story elements rewritten, userName debugged, and additional styling.

--- js/summary.js
+++ js/summary.js
@@ -47,13 +47,13 @@ var summaryText6 = document.getElementById('summary6');
 
 var summary1 = 'Phew, ' + userName + ', it is only 11am, and it appears you have had a ' + greeting + ' day. Let\'s look back at your morning so far.';
 
-var summary2 = 'You started your day going for a jog through the park and ran into a ' + color + ' ' + animal + '. ' + outcomeArray[0];
+var summary2 = 'You started your day off going for a jog through the park and encountered a ' + color + ' ' + animal + '. ' + outcomeArray[0];
 
-var summary3 = 'Next, you decided to head to the cafe, but you ran into some crazy traffic trying to cross a busy intersection. You mulled over whether you wanted to wait for the light to change or if you should just make a run for it. ' + outcomeArray[1];
+var summary3 = 'Next, you decided to head to your favorite café, but you ran into some crazy traffic trying to cross a busy intersection. You mulled over whether you wanted to wait for the light to change or if you should just make a run for it. ' + outcomeArray[1];
 
-var summary4 = 'You finally made it into the cafe where you had a tough decision on what to drink. Good thing there was a long line to give you time to decide. By the time you got to the front, you managed to narrow it down to two choices, ' + cold + ' or ' + hot + '.' + outcomeArray[2];
+var summary4 = 'You finally made it to the café where ' + outcomeArray[2];
 
-var summary5 = 'You made it through that ordeal and needed to grab a few groceries before heading home. You picked up some bread and ' + groceries + '. With your groceries in hand, you had one last decision to make, walk or take the bus. ' + outcomeArray[3];
+var summary5 = 'At the market you picked up some bread and ' + groceries + '. With your groceries in hand, you had one last decision to make, walk or take the bus. ' + outcomeArray[3];
 
 var summary6 = 'Wow, what a day. Now you only have one last choice to make....start your day over and try your luck again, or join your friends for lunch?';
 
